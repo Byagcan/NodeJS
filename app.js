9d1860a5a3fbba27457a2e8d9f85e9cc40ecc5c7
Buse <76453155+Byagcan@users.noreply.github.com>
2023-06-27 11:52:54 +0300

    Request and Response

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var http = require("http");
 
-function requestListener(request, response) {
-  //   response.writeHead(200, { "Content-Type": "text/plain" });
-  //   response.write("Hello World");
-  response.end();
-}
+var server = http.createServer((request, response) => {
+  //   console.log("Request method", request.method);
+  //   console.log("Requesr url", request.url)
+  //   console.log("Response status", response.statusCode);
 
-var server = http.createServer(requestListener);
+  response.setHeader("Content-Type", "text/plain");
+  response.statusCode = 200;
+  response.statusMessage = "OK";
+  response.write("Hello World");
+
+  response.end();
+});
 server.listen(3000);
 
 console.log("Server running at http://localhost:3000/");
