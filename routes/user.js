b71bce65fd7e6a9373b02fc72fbf46cbad68b525
Buse <76453155+Byagcan@users.noreply.github.com>
2023-06-27 18:33:56 +0300

    dinamik programming with ejsfor and ejs foreach

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,16 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
+const data = {
+  title: "Popüler Kurslar",
+  categories: [
+    "Web Programming",
+    "Mobile Programming",
+    "IOS Programming",
+    "AI Programming",
+  ],
+};
+
 router.use("/blogs/:blogid", function (req, res) {
   res.render("users/blog-details");
 });
@@ -11,7 +21,7 @@ router.use("/blog", function (req, res) {
 });
 
 router.use("/", function (req, res) {
-  res.render("users/index");
+  res.render("users/index", data);
 });
 
 module.exports = router;
