2bda13c9ca79c5c7f695832b0f142fd5fbd09d0a
Buse <76453155+Byagcan@users.noreply.github.com>
2023-06-27 11:57:08 +0300

    Routing Requests

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 var http = require("http");
 
 var server = http.createServer((request, response) => {
-  //   console.log("Request method", request.method);
-  //   console.log("Requesr url", request.url)
-  //   console.log("Response status", response.statusCode);
-
-  response.setHeader("Content-Type", "text/plain");
-  response.statusCode = 200;
-  response.statusMessage = "OK";
-  response.write("Hello World");
-
-  response.end();
+  if (request.url === "/") {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write("<h1>Home Page</h1>");
+    response.end();
+  } else if (request.url === "/about") {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write("<h1>About Page</h1>");
+    response.end();
+  } else {
+    response.writeHead(404, { "Content-Type": "text/html" });
+    response.write("<h1>Page Not Found</h1>");
+    response.end();
+  }
 });
 server.listen(3000);
 
