77120d0776d3b2a0a93ff5eede674f96bebfa93f
Buse <76453155+Byagcan@users.noreply.github.com>
2023-06-27 16:35:06 +0300

    add routes for admin and users

--- index.js
+++ index.js
@@ -2,23 +2,16 @@ const express = require("express");
 const app = express();
 
 const path = require("path");
+const userRoutes = require("./routes/user");
+const adminRoutes = require("./routes/admin");
 
 app.use("/libs", express.static(path.join(__dirname, "node_modules")));
 app.use("/static", express.static(path.join(__dirname, "public")));
 
+app.use("/admin", adminRoutes);
+app.use(userRoutes);
+
 //en özel route en üstte olmalı
-
-app.use("/blogs/:blogid", function (req, res) {
-  res.sendFile(path.join(__dirname, "views/users", "blog-details.html"));
-});
-
-app.use("/blog", function (req, res) {
-  res.sendFile(path.join(__dirname, "views/users", "blogs.html"));
-});
-
-app.use("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "views/users", "index.html"));
-});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
