24b9aed918ae79842603c72a703603b3a3b167be
Buse <76453155+Byagcan@users.noreply.github.com>
2023-06-27 13:31:00 +0300

    routing with app.use

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
 
-app.use(function (req, res) {
-  res.end("Hello World");
+//en özel route en üstte olmalı
+
+app.use("/about/:aboutid", function (req, res) {
+  console.log(req.params);
+  res.send("about detail");
+});
+
+app.use("/about", function (req, res) {
+  res.send("about");
+});
+
+app.use("/", function (req, res) {
+  res.send("home");
 });
 
 app.listen(3000, () => {
