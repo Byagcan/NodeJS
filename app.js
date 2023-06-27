de2541b093f54737133fd967e20e101df339c4c0
Buse <76453155+Byagcan@users.noreply.github.com>
2023-06-27 12:38:51 +0300

    Form Data

--- app.js
+++ app.js
@@ -15,15 +15,27 @@ var server = http.createServer((request, response) => {
       response.end();
     });
   } else if (request.url === "/create" && request.method === "POST") {
-    fs.appendFile("about.txt", "deneme", (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        response.statusCode = 302;
-        response.setHeader("Location", "/");
-        response.end();
-      }
+    const data = [];
+    request.on("data", (chunk) => {
+      data.push(chunk);
     });
+
+    request.on("end", () => {
+        const body = Buffer.concat(data).toString();
+        const parsedBody = body.split('=')[1];
+
+        fs.appendFile("about.txt", parsedBody, (err) => {
+            if (err) {
+              console.log(err);
+            } else {
+              response.statusCode = 302;
+              response.setHeader("Location", "/");
+              response.end();
+            }
+          });
+    });
+
+    
   } else if (request.url === "/create") {
     fs.readFile("create.html", (err, html) => {
       response.writeHead(200, { "Content-Type": "text/html" });
